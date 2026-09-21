5cd41c2703d8adaf0730c7bf5e6e1f53896eadd7
Turbo <mmn05460@gmail.com>
2026-09-21 13:26:57 +0330

    Intial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const repobut = document.getElementById("repo");
+const inso = document.getElementById("downsou");
+const tutorial = document.getElementById("Tutr");
+const maintutorial = document.getElementById("mtu");
+
+repobut.addEventListener("click",function (){window.open("https://github.com/Turbo-Dev-C/SemiColon","_blank")});
+inso.addEventListener("click",function (){window.open("instalsrc.html","_blank")});
+tutorial.addEventListener("click",function (){window.open("tutorial/menu.html","_blank")});
+maintutorial.addEventListener("click",function (){window.open("tutorial/menu.html","_blank")});
